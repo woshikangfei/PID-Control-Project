44d4eee8aef8e99982383c727d4aab5e2d163e92
kangfei <kang_chao520@163.com>
2017-06-03 15:14:01 +0800

    Update PID.cpp
    modfiy

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,12 +11,34 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  // error term for P - assume very large initially
+  p_error = numeric_limits<double>::max()/100;
+
+  // error term for I - assume total area as very small as car has not started yet
+  i_error = 0.0001;
+
+  // error term for D - assume initial position of car is very far from CTE
+  d_error = 0.0001;
 }
 
 void PID::UpdateError(double cte) {
+   if (p_error == numeric_limits<double>::max()/100){
+      p_error = cte;
+  }
+    
+
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
+
 double PID::TotalError() {
+   return Kp*p_error + Kd*d_error + Ki*i_error;
 }
 
 
